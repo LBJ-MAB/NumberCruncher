bd3a5b85c3ea2a702465542e9c1295432884fcb4
KEYFSLTD\Laurie.Bassett-Jones <laurie.bassett-jones@mab.org.uk>
2025-10-07 10:13:27 +0100

    made median method generic

--- Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
+++ Core/obj/Debug/net8.0/Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12e19eecc99fedd6d5937ab6af4862184466a32c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2e63744c923896483dcd5f46c40b68db7d757b3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
